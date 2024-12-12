c01753d74ab21839d5917726f5163fb57b9f039f
rcandiaallende <rcandiaallende@gmail.com>
2024-12-12 10:51:17 -0300

    fix nc

--- listado_notas_credito_report/models/listado_notas_credito.py
+++ listado_notas_credito_report/models/listado_notas_credito.py
@@ -12,10 +12,13 @@ class ListadoNotasCreditoReportWizard(models.TransientModel):
     date_start = fields.Date(string="Fecha Inicio", required=True, default=fields.Date.today)
     date_end = fields.Date(string="Fecha Final", required=True, default=fields.Date.today)
     partner_id = fields.Many2one('res.partner', string="Cliente")
-    show_invoices = fields.Boolean('Mostrar facturas de origen anuladas o modificadas(conciliadas)', default=True)
+    show_invoices = fields.Boolean('Mostrar facturas de origen anuladas o modificadas (conciliadas)', default=True)
 
     @api.multi
     def get_report(self):
+        if self.date_start > self.date_end:
+            raise ValueError("La fecha de inicio no puede ser mayor a la fecha final.")
+
         data = {
             'ids': self.ids,
             'model': self._name,
@@ -23,12 +26,17 @@ class ListadoNotasCreditoReportWizard(models.TransientModel):
                 'date_start': self.date_start,
                 'date_end': self.date_end,
                 'partner_id': self.partner_id.id,
+                'show_invoices': self.show_invoices,
             },
         }
+
+        # Generar ambas opciones de informe
+        report_refs = []
         if self.show_invoices:
-            return self.env.ref('listado_notas_credito_report_sifen.recap_report_sifen').report_action(self, data=data)
-        else:
-            return self.env.ref('listado_notas_credito_report.recap_report').report_action(self, data=data)
+            report_refs.append(self.env.ref('listado_notas_credito_report_sifen.recap_report_sifen').report_action(self, data=data))
+        report_refs.append(self.env.ref('listado_notas_credito_report.recap_report').report_action(self, data=data))
+
+        return report_refs
 
 
 class ReportListadoNotasCredito(models.AbstractModel):
@@ -39,28 +47,33 @@ class ReportListadoNotasCredito(models.AbstractModel):
     def _get_report_values(self, docids, data=None):
         date_start = data['form']['date_start']
         date_end = data['form']['date_end']
+        partner_id = data['form'].get('partner_id')
+        show_invoices = data['form'].get('show_invoices', True)
+
         date_start_obj = datetime.strptime(date_start, DATE_FORMAT)
         date_end_obj = datetime.strptime(date_end, DATE_FORMAT)
-        partner_id = data['form']['partner_id']
-
 
         start_report = date_start_obj.strftime('%d/%m/%Y')
         end_report = date_end_obj.strftime('%d/%m/%Y')
 
-        if partner_id:
-            facturas = self.env['account.invoice'].search(
-                [('type', '=', 'out_refund'), ('state', 'in', ['open', 'paid', 'cancel']),
-                 ('date_invoice', '>=', date_start_obj.strftime(DATETIME_FORMAT)),
-                 ('date_invoice', '<=', date_end_obj.strftime(DATETIME_FORMAT)),
-                 ('tax_line_ids', '!=', False)]).filtered(lambda x: x.partner_id.id == partner_id or x.partner_id.parent_id.id == partner_id)
-        else:
-            facturas = self.env['account.invoice'].search(
-                [('type', '=', 'out_refund'), ('state', 'in', ['open', 'paid', 'cancel']),
-                 ('date_invoice', '>=', date_start_obj.strftime(DATETIME_FORMAT)),
-                 ('date_invoice', '<=', date_end_obj.strftime(DATETIME_FORMAT)), ('tax_line_ids', '!=', False)])
+        domain = [
+            ('type', '=', 'out_refund'),
+            ('state', 'in', ['open', 'paid', 'cancel']),
+            ('date_invoice', '>=', date_start_obj.strftime(DATETIME_FORMAT)),
+            ('date_invoice', '<=', date_end_obj.strftime(DATETIME_FORMAT)),
+            ('tax_line_ids', '!=', False),
+        ]
 
-        docs = sorted(facturas, key=lambda x: x.fake_number)
-        docs = sorted(docs, key=lambda x: x.date_invoice)
+        if partner_id:
+            domain.append(('partner_id', 'child_of', partner_id))
+
+        facturas = self.env['account.invoice'].search(domain)
+
+        if show_invoices:
+            facturas = facturas.filtered(lambda x: x.matched_invoice_ids)
+
+        docs = sorted(facturas, key=lambda x: (x.date_invoice, x.fake_number))
+
         return {
             'doc_ids': data['ids'],
             'doc_model': data['model'],
