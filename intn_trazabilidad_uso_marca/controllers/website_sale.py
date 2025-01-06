86b4eae901bca78bff2742b227bf8fc96ac409bb
segel <hugo.benegas@segel.com.py>
2025-01-06 09:13:38 -0300

    [DEV] Ajustes varios normas

--- intn_trazabilidad_uso_marca/controllers/website_sale.py
+++ intn_trazabilidad_uso_marca/controllers/website_sale.py
@@ -71,14 +71,6 @@ class WebsiteSaleInherit(WebsiteSale):
             # Crear la factura
             invoice = self._create_invoice_v12(order)
             if invoice:
-                attachment = request.env['ir.attachment'].sudo().create({
-                    'name': 'Comprobante_transferencia',
-                    'type': 'binary',
-                    'datas': order.ecommerce_payment_receipt,
-                    'res_model': 'account.invoice',
-                    'res_id': invoice.id,
-                })
-                invoice.ecommerce_payment_receipt = order.ecommerce_payment_receipt
                 _logger.info(f"Factura creada y publicada para la orden {order.name}: {invoice.number}")
 
             # Crear y enviar el correo
@@ -107,6 +99,14 @@ class WebsiteSaleInherit(WebsiteSale):
         try:
             invoice_id = sale_order.sudo().action_invoice_create(grouped=True)
             invoice = request.env['account.invoice'].sudo().browse(invoice_id)
+            attachment = request.env['ir.attachment'].sudo().create({
+                'name': 'Comprobante_transferencia',
+                'type': 'binary',
+                'datas': sale_order.ecommerce_payment_receipt,
+                'res_model': 'account.invoice',
+                'res_id': invoice.id,
+            })
+            invoice.ecommerce_payment_receipt = sale_order.ecommerce_payment_receipt
             invoice.sudo().action_move_create()
             invoice.sudo().invoice_validate()
             return invoice
